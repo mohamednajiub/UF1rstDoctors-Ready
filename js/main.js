be3fd76645f855090c9283a82eaa52839d071147
Mohamed Emad <m.emad550@gmail.com>
2018-07-26 00:42:17 +0200

    UI update

--- js/main.js
+++ js/main.js
@@ -15,21 +15,3 @@ document.addEventListener('DOMContentLoaded', () => {
     topLayer.style.width = e.clientX + skew + delta + 'px';
   })
 });
-
-$('#recipeCarousel').carousel({
-  interval: 5000
-});
-
-$('#recipeCarousel .carousel .carousel-item').each(function () {
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  if (next.next().length > 0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});
