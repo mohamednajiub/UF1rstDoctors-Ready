060d7414e2edcb6cebf50e9515e8afa519382728
Mohamed Najiub <mohamednajiub@gmail.com>
2021-07-23 01:45:39 +0200

    feat: update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,65 @@
-const gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    imagemin = require('gulp-imagemin');
-// html
-gulp.task('build', () => {
-    gulp.src('index.html')
-        .pipe(gulp.dest('./build/'));
-});
-// compiling sass to css
-gulp.task('styles', () => {
-    gulp.src([
-            'node_modules/bootstrap/scss/bootstrap.scss',
-            'sass/**/*.sass'
-        ])
-        .pipe(sass())
-        .on('error', (err) => {
-            console.log(err.message);
-        })
-        .pipe(gulp.dest('./build/css'));
-});
+var gulp = require("gulp"),
+  uglify = require("gulp-uglify"),
+  //   sass = require("gulp-sass"),
+  gulpSass = require("gulp-sass"),
+  nodeSass = require("node-sass"),
+  imagemin = require("gulp-imagemin");
 
-// minifying js script
-gulp.task('scripts', () => {
-    gulp.src([
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/jquery/dist/jquery.min.js',
-            'node_modules/popper.js/dist/umd/popper.min.js',
-            'js/*.js'
-        ])
-        .pipe(gulp.dest('./build/js'));
-});
+const sass = gulpSass(nodeSass);
 
-// compress images
-gulp.task('image', () => {
-    gulp.src('img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./build/img'));
-});
+const build = () => {
+  return gulp.src("index.html").pipe(gulp.dest("./build/"));
+};
 
-// font awesome
-gulp.task('fontawesome', () => {
-    gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-        .pipe(gulp.dest('./build/css'));
-});
+const styles = () => {
+  return gulp
+    .src(["node_modules/bootstrap/scss/bootstrap.scss", "sass/**/*.scss"])
+    .pipe(sass())
+    .on("error", (err) => {
+      console.log(err.message);
+    })
+    .pipe(gulp.dest("./build/css"));
+};
 
-// fonts
-gulp.task('fonts', () => {
-    gulp.src('node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest('./build/fonts'))
-});
+const scripts = () => {
+  return gulp
+    .src([
+      "node_modules/bootstrap/dist/js/bootstrap.min.js",
+      "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/popper.js/dist/umd/popper.min.js",
+      "js/*.js",
+    ])
+    .pipe(gulp.dest("./build/js"));
+};
 
-// watching files
-gulp.task('watch', () => {
-    gulp.watch('index.html', ['build']);
-    gulp.watch([
-        'node_modules/bootstrap/scss/bootstrap.scss',
-        'sass/**/*.sass'
-    ], ['styles']);
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('img/**/*', ['image']);
-});
+const image = () => {
+  return gulp.src("img/**/*").pipe(imagemin()).pipe(gulp.dest("./build/img"));
+};
 
+const fontAwesome = () => {
+  return gulp
+    .src("node_modules/font-awesome/css/font-awesome.min.css")
+    .pipe(gulp.dest("./build/css"));
+};
 
+const fonts = () => {
+  return gulp
+    .src("node_modules/font-awesome/fonts/*")
+    .pipe(gulp.dest("./build/fonts"));
+};
 
-gulp.task('default', [
-    'build',
-    'styles',
-    'scripts',
-    'image',
-    'fontawesome',
-    'fonts',
-    'watch'
-])
+const watch = () => {
+  gulp.watch("index.html", [build]);
+  gulp.watch(
+    ["node_modules/bootstrap/scss/bootstrap.scss", "sass/**/*.scss"],
+    [styles]
+  );
+  gulp.watch("js/*.js", [scripts]);
+  gulp.watch("img/**/*", [image]);
+};
+
+const dev = gulp.series(
+  gulp.series(build, styles, image, scripts, fontAwesome, fonts)
+);
+
+exports.default = dev;
